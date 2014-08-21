91d3876f166fd256c4e59ce8c69ef226f1cd1f37
m.strozynski@radytek.com <m.strozynski@radytek.com>
2014-08-21 11:11:45 +0200

    Collect site error messages in one place

---
+++ ExternalWebTests/src/test/ErrorMessages.java
@@ -0,0 +1,8 @@
+package test;
+
+public class ErrorMessages {
+	static String AddressInUse = "address is already in use";
+	static String AddressInvalid = "address is invalid";
+	static String EnterAddress = "enter your email address";
+	static String EnterAddress2 = "enter an email address";
+}
