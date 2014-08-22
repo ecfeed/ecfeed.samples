3a0f94dbbda207a80070384e7d2cb67d2bcf026a
m.strozynski@radytek.com <m.strozynski@radytek.com>
2014-08-22 12:37:42 +0200

    Add page address class

---
+++ ExternalWebTests/src/tools/PageAddress.java
@@ -0,0 +1,7 @@
+package tools;
+
+public class PageAddress {
+	public static String Base = "http://localhost:8080";
+	public static String Login = "http://localhost:8080/login";
+	public static String Register = "https://localhost:8443/register";
+}
