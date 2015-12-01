b629dcf64ac9ff28774a2ff10e43f7ecb7d58f71
Marek <m.kuczabinski@radytek.com>
2015-12-01 10:10:22 +0100

    CalculatorTest - initial project.

---
+++ selenium/Calculator Tutorial/CalculatorTest/src/com/example/test/TestCalculator.java
@@ -0,0 +1,11 @@
+package com.example.test;
+
+public class TestCalculator {
+
+	public void testCalculator() {
+		// TODO Auto-generated method stub
+		System.out.println("testCalculator()");
+	}
+
+
+}
