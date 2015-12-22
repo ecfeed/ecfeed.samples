93fa19b260611b756e2371df98cfa9f513492107
Marek <m.kuczabinski@radytek.com>
2015-12-22 08:27:05 +0100

    Broadleaf DemoSite-4-0-5 eclipse workspace added. Previous DemoSite renamed to Old.

---
+++ selenium/Broadleaf Commerce Case Study/DemoSiteOld/site/src/main/java/com/mycompany/controller/checkout/HCRegisterCustomerForm.java
@@ -0,0 +1,17 @@
+package com.mycompany.controller.checkout;
+
+import org.broadleafcommerce.profile.web.core.form.RegisterCustomerForm;
+
+public class HCRegisterCustomerForm extends RegisterCustomerForm {
+
+	public String referralCode;
+
+	public String getReferralCode() {
+		return referralCode;
+	}
+
+	public void setReferralCode(String referralCode) {
+		this.referralCode = referralCode;
+	}
+
+}
