10898de083c8c445215b0e80c394679a5b30f8a0
Marek Kuczabinski <m.kuczabinski@radytek.com>
2016-07-22 08:34:19 +0200

    FlightBooking - basic fields.

--- android/FlightBooking/gen/com/ecfeed/flightbooking/R.java
+++ android/FlightBooking/gen/com/ecfeed/flightbooking/R.java
@@ -25,14 +25,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int comment=0x7f080006;
-        public static final int multiplyButton=0x7f080004;
-        public static final int result=0x7f080005;
-        public static final int val1=0x7f080000;
-        public static final int val2=0x7f080001;
-        public static final int val3=0x7f080002;
-        public static final int val4=0x7f080003;
+        public static final int action_settings=0x7f08000c;
+        public static final int editText1=0x7f08000b;
+        public static final int editTextFlyOut=0x7f080009;
+        public static final int editTextFrom=0x7f080001;
+        public static final int editTextTo=0x7f080003;
+        public static final int radioGroup1=0x7f080004;
+        public static final int radioOneWay=0x7f080005;
+        public static final int radioReturn=0x7f080006;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080002;
+        public static final int textView3=0x7f080008;
+        public static final int textView4=0x7f080007;
+        public static final int textView5=0x7f08000a;
     }
     public static final class layout {
         public static final int main=0x7f030000;
