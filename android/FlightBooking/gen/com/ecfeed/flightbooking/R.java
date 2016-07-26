110f63ec7adf09ee3925fbad447a0704291b8cfa
Marek Kuczabinski <m.kuczabinski@radytek.com>
2016-07-26 14:16:40 +0200

    Unused fields removed.

--- android/FlightBooking/gen/com/ecfeed/flightbooking/R.java
+++ android/FlightBooking/gen/com/ecfeed/flightbooking/R.java
@@ -28,18 +28,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000b;
-        public static final int comment=0x7f09000a;
+        public static final int action_settings=0x7f090004;
         public static final int from_spinner=0x7f090001;
-        public static final int multiplyButton=0x7f090008;
-        public static final int result=0x7f090009;
         public static final int textView1=0x7f090002;
         public static final int to_spinner=0x7f090003;
         public static final int txFrom=0x7f090000;
-        public static final int val1=0x7f090004;
-        public static final int val2=0x7f090005;
-        public static final int val3=0x7f090006;
-        public static final int val4=0x7f090007;
     }
     public static final class layout {
         public static final int main=0x7f030000;
