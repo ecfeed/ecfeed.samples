e819cc5a8dc707944c2f38ad16b7868129c9ac04
Krzysztof Skorupski <k.skorupski@testify.no>
2019-11-08 12:42:52 +0100

    selenium SkyScanner

--- workshop/JUnit5/src/test/java/other/selenium/SkyScanner.java
+++ workshop/JUnit5/src/test/java/other/selenium/SkyScanner.java
@@ -1,17 +1,18 @@
 package other.selenium;
 
+import com.ecfeed.junit.annotation.EcFeedInput;
+import com.ecfeed.junit.annotation.EcFeedModel;
+import com.ecfeed.junit.annotation.EcFeedTest;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 
-import java.util.Arrays;
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -24,9 +25,9 @@ public class SkyScanner {
 
 //------------------------------------------------------------------------------
 
-    private static void delay() {
+    private static void delay(int milliseconds) {
         try {
-            Thread.sleep(1000);
+            Thread.sleep(milliseconds);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -34,44 +35,86 @@ public class SkyScanner {
 
 //------------------------------------------------------------------------------
 
+    private static RemoteWebDriver driver;
+    private static Wait<RemoteWebDriver> driverWait;
+
     @BeforeAll
     static void beforeAll() {
         System.setProperty("webdriver.gecko.driver", webDriver);
+        driver = new FirefoxDriver();
+
+        driverWait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(5))
+                .ignoring(NoSuchElementException.class);
+
+        driver.get(webPageAddress);
     }
 
-    @Test
-    void seleniumValidate() {
+    @AfterAll
+    static void afterAll() {
+         driver.quit();
+    }
 
-        FirefoxDriver driver = new FirefoxDriver();
-        driver.get(webPageAddress);
+    @EcFeedTest
+    @EcFeedModel("4408-1001-3225-0620-6211")
+    @EcFeedInput("'method':'com.ecfeed.SkyScanner.execute', 'dataSource':'genNWise', 'constraints':'ALL'")
+    void seleniumValidate(String fFrom, String fTo, String fDepart, String fReturn) {
 
-//        delay();
-
-        CharSequence[] clear = {Keys.BACK_SPACE, Keys.BACK_SPACE,Keys.BACK_SPACE,Keys.BACK_SPACE,Keys.BACK_SPACE,Keys.BACK_SPACE,Keys.BACK_SPACE,Keys.BACK_SPACE,Keys.BACK_SPACE,Keys.BACK_SPACE,Keys.BACK_SPACE,Keys.BACK_SPACE,Keys.BACK_SPACE,Keys.BACK_SPACE,Keys.BACK_SPACE};
+        CharSequence[] clear = {Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE,
+                                Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE,
+                                Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE,
+                                Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE,
+                                Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE,
+                                Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE,
+                                Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE};
 
         WebElement element;
+
+        element = driverWait.until(driver -> driver.findElement(By.id("simplified-search-controls-origin")));
+        if (element.getAttribute("value").equals(fFrom)) {
+            System.out.println("Ignoring: flight origin");
+        } else {
+            element.sendKeys(clear);
+            element.sendKeys(fFrom);
+            element.sendKeys(Keys.TAB);
+        }
+
+        element = driverWait.until(driver -> driver.findElement(By.id("simplified-search-controls-destination")));
+        if (element.getAttribute("value").equals(fTo)) {
+            System.out.println("Ignoring: flight destination");
+        } else {
+            element.sendKeys(clear);
+            element.sendKeys(fTo);
+            element.sendKeys(Keys.TAB);
+        }
+
+        element = driverWait.until(driver -> driver.findElement(By.id("simplified-search-controls-depart-date")));
+        driver.executeScript("arguments[0].click()", element);
+
+        element = driverWait.until(driver -> driver.findElement(By.className("monthselector")));
+        element = driverWait.until(ExpectedConditions.elementToBeClickable(element.findElement(By.xpath("//*[contains(text(), '" + fDepart + "')]"))));
+        driver.executeScript("arguments[0].click()", element);
+
+        element = driverWait.until(driver -> driver.findElement(By.id("simplified-search-controls-return-date")));
+        driver.executeScript("arguments[0].click()", element);
+
+        element = driverWait.until(driver -> driver.findElement(By.className("monthselector")));
+        element = driverWait.until(ExpectedConditions.elementToBeClickable(element.findElement(By.xpath("//*[contains(text(), '" + fReturn + "')]"))));
+        driver.executeScript("arguments[0].click()", element);
+
+        delay(2000);
+
+        element = driverWait.until(driver -> driver.findElement(By.id("destinations")).findElement(By.className("browse-data-entry")));
+
         try {
-            element = driver.findElementById("simplified-search-controls-origin");
-            element.sendKeys(clear);
-            element.sendKeys("Oslo Gardermoen (OSL)");
-            element.sendKeys(Keys.TAB);
-
-            element = driver.findElementById("simplified-search-controls-destination");
-            element.sendKeys(clear);
-            element.sendKeys("Wroclaw (WRO)");
-            element.sendKeys(Keys.TAB);
-
-            delay();
-
-            element = driver.findElementById("destinations").findElement(By.className("browse-data-entry"));
             String destination = element.findElement(By.cssSelector("h3")).getText();
             String price = element.findElement(By.className("price")).getText().split(" ")[1];
-            System.out.println(destination + " " + price);
-
-        } catch (NoSuchElementException e) {
+            System.out.println(destination.toLowerCase() + ": " + price.toLowerCase());
+        } catch (Exception e) {
+            System.out.println("The location is not available");
             fail();
         }
 
-        driver.quit();
     }
 }
