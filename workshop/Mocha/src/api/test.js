607d18adf06d83c11ce507572cd2975b5fab80ac
krzysztof skorupski <k.skorupski@testify.no>
2021-07-08 16:11:36 +0200

    node feedback selenium

--- workshop/Mocha/src/api/test.js
+++ workshop/Mocha/src/api/test.js
@@ -9,7 +9,7 @@ const fTest = 'com.example.test.Demo.typeString';
 const data = [] 
 
 before(async () => {
-    for await (const response of testProvider.generateNWise(fTest, { feedback: true, label:'API', constraints: "NONE"})) { data.push(response) }
+    for await (const response of testProvider.generateNWise(fTest, { feedback: true, label:'API', constraints: "ALL"})) { data.push(response) }
 })
 
 it('API tests', () => {
@@ -22,16 +22,19 @@ it('API tests', () => {
             }
 
             let res = await axios.post('https://api.ecfeed.com', {}, { params });
-
+           
             let index = 1;
             let custom = {};
 
-            res.data.errorOutput.forEach(e => { custom[`${index++} / Output`] = e; })
-            res.data.errorInput.forEach(e => { custom[`${index++} / Input`] = e; })
-           
-            if (Object.keys(custom).length > 0) {
-                let comment = res.data.errorOutput.length > 0 && res.data.errorInput.length > 0 ? "Input/Output" : res.data.errorOutput.length > 0 ? "Output" : "Input";
-                testHandle.addFeedback(false, { custom, comment });
+            if (res.data.errorInput.length > 0) {
+                res.data.errorInput.forEach(e => { custom[`${index++}`] = e; })
+                testHandle.addFeedback(false, { custom, comment: "Input" });
+                assert.fail();
+            }
+
+            if (res.data.errorOutput.length > 0) {
+                res.data.errorOutput.forEach(e => { custom[`${index++}`] = e; })
+                testHandle.addFeedback(false, { custom, comment: "Output" });
                 assert.fail();
             }
 
--- workshop/Mocha/src/selenium/test.js
+++ workshop/Mocha/src/selenium/test.js
@@ -1,145 +1,110 @@
-const TestProvider = require('../../../TestProvider').TestProvider;
+const TestProvider = require('ecfeed').TestProvider; 
 const forEach = require('mocha-each');
 const assert = require('assert');
+const axios = require('axios');
+const {Builder, By, Key, until, Browser} = require('selenium-webdriver');
 
-const data = {}    
+const testProvider = new TestProvider("6EG2-YL4S-LMAK-Y5VW-VPV9");
+const testMethod = 'com.example.test.Demo.typeString';
+const webpage = 'http://www.workshop-2020-march.ecfeed.com?mode=error';
 
-const testProvider = new TestProvider("IMHL-K0DU-2U0I-J532-25J9");
-const f10x10 = 'com.example.test.Playground.size_10x10';
-const f100x2 = 'com.example.test.Playground.size_100x2';
-const fTest = 'QuickStart.test';
+let data = [] 
+let driver = null;
+
+pageFormInput = [["name", "address", "quantity", "phone", "email"], ["country", "product", "color", "size", "payment", "delivery"]];
+pageFormOutput = ["status", "response"];
+pageFormControl = ["submit"];
 
 before(async () => {
-    await assemble("10x10 / Random / Quantity - Single", testProvider.generateRandom(f10x10, { feedback:true, length:1, label:'Random / Quantity - Single'}));
-    await assemble("10x10 / Random / Quantity - Short", testProvider.generateRandom(f10x10, { feedback:true, length:(100 + Math.floor(Math.random() * 400)), label:'Random / Quantity - Short'}));
-    await assemble("10x10 / Random / Quantity - Long", testProvider.generateRandom(f10x10, { feedback:true, length:(1000 + Math.floor(Math.random() * 4000)), label:'Random / Quantity - Long'}));
-    await assemble("10x10 / Random / Custom", testProvider.generateRandom(f10x10, { feedback:true, length:1, custom:{ key1: 'value1', key2: 'value2' }, label:'Random / Custom'}));
-    await assemble("10x10 / NWise", testProvider.generateNWise(f10x10, { feedback:true, label:'NWise'}));
-
-    await assemble("100x2 / Random / Quantity - Single", testProvider.generateRandom(f100x2, { feedback:true, length:1, label:'Random / Quantity - Single'}));
-    await assemble("100x2 / Random / Quantity - Short", testProvider.generateRandom(f100x2, { feedback:true, length:(100 + Math.floor(Math.random() * 400)), label:'Random / Quantity - Short'}));
-    await assemble("100x2 / Random / Quantity - Long", testProvider.generateRandom(f100x2, { feedback:true, length:(1000 + Math.floor(Math.random() * 4000)), label:'Random / Quantity - Long'}));
-    await assemble("100x2 / Random / Custom", testProvider.generateRandom(f100x2, { feedback: true,length:1, custom:{ key1: 'value1', key2: 'value2' }, label:'Random / Custom'}));
-    await assemble("100x2 / NWise", testProvider.generateNWise(f100x2, { feedback:true, label:'NWise'}));
-
-    await assemble("fTest / Random / Quantity - Single", testProvider.generateRandom(fTest, { feedback:true, length:1, label:'Random / Quantity - Single'}));
-    await assemble("fTest / Random / Quantity - Short", testProvider.generateRandom(fTest, { feedback:true, length:(100 + Math.floor(Math.random() * 400)), label:'Random / Quantity - Short'}));
-    await assemble("fTest / Random / Quantity - Long", testProvider.generateRandom(fTest, { feedback:true, length:(1000 + Math.floor(Math.random() * 4000)), label:'Random / Quantity - Long'}));
-    await assemble("fTest / Random", testProvider.generateRandom(fTest, { feedback:true, label:'Random'}));
-    await assemble("fTest / Random - Adaptive", testProvider.generateRandom(fTest, { feedback:true, length:10, adaptive:false, label:'Random - Adaptive'}));
-    await assemble("fTest / Random - Duplicates", testProvider.generateRandom(fTest, { feedback:true, length:10, duplicates:true, label:'Random - Duplicates'}));
-    await assemble("fTest / NWise", testProvider.generateNWise(fTest, { feedback:true, label:'NWise'}));
-    await assemble("fTest / NWise - N", testProvider.generateNWise(fTest, { feedback:true, n:3, label:'NWise - N'}));
-    await assemble("fTest / NWise - Coverage", testProvider.generateNWise(fTest, { feedback:true, coverage:50, label:'NWise - Coverage'}));
-    await assemble("fTest / NWise / Constraints - None", testProvider.generateNWise(fTest, { feedback:true, constraints:"NONE", label:'NWise / Constraints - None'}));
-    await assemble("fTest / NWise / Constraints - Selected", testProvider.generateNWise(fTest, { feedback:true, constraints:['constraint1', 'constraint2'], label:'NWise / Constraints - Selected'}));
-    await assemble("fTest / NWise / Choices - Selected", testProvider.generateNWise(fTest, { feedback:true, choices:{arg1: ['choice1', 'choice2'], arg2: ['choice2', 'choice3']}, label:'NWise / Choices - Selected'}));
-    await assemble("fTest / Cartesian", testProvider.generateCartesian(fTest, { feedback:true, label:'Cartesian'}));
-    await assemble("fTest / Static", testProvider.generateStatic(fTest, { feedback:true, label:'Static'}));
-    await assemble("fTest / Static - All", testProvider.generateStatic(fTest, { feedback:true, testSuites:"ALL", label:'Static - All'}));
-    await assemble("fTest / Static - Selected", testProvider.generateStatic(fTest, { feedback:true, testSuites:['suite1'], label:'Static - Selected'}));
+    for await (const response of testProvider.generateNWise(testMethod, { feedback: true, label:'Selenium', constraints: "ALL"})) { data.push(response) }
+    driver = await new Builder().forBrowser('firefox').build();
+    await driver.get(webpage);
 })
 
-it('Mocha tests', () => {
-    describe('f10x10', async () => {
-        forEach(data['10x10 / Random / Quantity - Single']).it("Random / Quantity - Single", (a, b, c, d, e, f, g, h, i, j, testHandle) => { validateF10x10(a, b, c, d, e, f, g, h, i, j, testHandle) });
-        forEach(data['10x10 / Random / Quantity - Short']).it("Random / Quantity - Short", (a, b, c, d, e, f, g, h, i, j, testHandle) => { validateF10x10(a, b, c, d, e, f, g, h, i, j, testHandle) });
-        forEach(data['10x10 / Random / Quantity - Long']).it("Random / Quantity - Long", (a, b, c, d, e, f, g, h, i, j, testHandle) => { validateF10x10(a, b, c, d, e, f, g, h, i, j, testHandle) });
-        forEach(data['10x10 / Random / Custom']).it("Random / Custom", (a, b, c, d, e, f, g, h, i, j, testHandle) => { validateF10x10(a, b, c, d, e, f, g, h, i, j, testHandle) });
-        forEach(data['10x10 / NWise']).it("NWise", (a, b, c, d, e, f, g, h, i, j, testHandle) => { validateF10x10(a, b, c, d, e, f, g, h, i, j, testHandle) });
-    });
-    describe('f100x2', async () => {
-        forEach(data['100x2 / Random / Quantity - Single']).it("Random / Quantity - Single", (a, b, testHandle) => { validateF100x2(a, b, testHandle) });
-        forEach(data['100x2 / Random / Quantity - Short']).it("Random / Quantity - Short", (a, b, testHandle) => { validateF100x2(a, b, testHandle) });
-        forEach(data['100x2 / Random / Quantity - Long']).it("Random / Quantity - Long", (a, b, testHandle) => { validateF100x2(a, b, testHandle) });
-        forEach(data['100x2 / Random / Custom']).it("Random / Custom", (a, b, testHandle) => { validateF100x2(a, b, testHandle) });
-        forEach(data['100x2 / NWise']).it("NWise", (a, b, testHandle) => { validateF100x2(a, b, testHandle) });
-    });
-    describe('fTest', async () => {
-        forEach(data['fTest / Random / Quantity - Single']).it("Quantity - Single", (arg1, arg2, arg3, testHandle) => { validateFTest(arg1, arg2, arg3, testHandle) });
-        forEach(data['fTest / Random / Quantity - Short']).it("Quantity - Short", (arg1, arg2, arg3, testHandle) => { validateFTest(arg1, arg2, arg3, testHandle) });
-        forEach(data['fTest / Random / Quantity - Long']).it("Quantity - Long", (arg1, arg2, arg3, testHandle) => { validateFTest(arg1, arg2, arg3, testHandle) });
-        forEach(data['fTest / Random']).it("Random", (arg1, arg2, arg3, testHandle) => { validateFTest(arg1, arg2, arg3, testHandle) });
-        forEach(data['fTest / Random - Adaptive']).it("Random - Adaptive", (arg1, arg2, arg3, testHandle) => { validateFTest(arg1, arg2, arg3, testHandle) });
-        forEach(data['fTest / Random - Duplicates']).it("Random - Duplicates", (arg1, arg2, arg3, testHandle) => { validateFTest(arg1, arg2, arg3, testHandle) });
-        forEach(data['fTest / NWise']).it("NWise", (arg1, arg2, arg3, testHandle) => { validateFTest(arg1, arg2, arg3, testHandle) });
-        forEach(data['fTest / NWise - N']).it("NWise - N", (arg1, arg2, arg3, testHandle) => { validateFTest(arg1, arg2, arg3, testHandle) });
-        forEach(data['fTest / NWise - Coverage']).it("NWise - Coverage", (arg1, arg2, arg3, testHandle) => { validateFTest(arg1, arg2, arg3, testHandle) });
-        forEach(data['fTest / NWise / Constraints - None']).it("NWise / Constraints - None", (arg1, arg2, arg3, testHandle) => { validateFTest(arg1, arg2, arg3, testHandle) });
-        forEach(data['fTest / NWise / Constraints - Selected']).it("NWise / Constraints - Selected", (arg1, arg2, arg3, testHandle) => { validateFTest(arg1, arg2, arg3, testHandle) });
-        forEach(data['fTest / NWise / Choices - Selected']).it("NWise / Choices - Selected", (arg1, arg2, arg3, testHandle) => { validateFTest(arg1, arg2, arg3, testHandle) });
-        forEach(data['fTest / Cartesian']).it("Cartesian", (arg1, arg2, arg3, testHandle) => { validateFTest(arg1, arg2, arg3, testHandle) });
-        forEach(data['fTest / Static']).it("Static", (arg1, arg2, arg3, testHandle) => { validateFTest(arg1, arg2, arg3, testHandle) });
-        forEach(data['fTest / Static - All']).it("Static - All", (arg1, arg2, arg3, testHandle) => { validateFTest(arg1, arg2, arg3, testHandle) });
-        forEach(data['fTest / Static - Selected']).it("Static - Selected", (arg1, arg2, arg3, testHandle) => { validateFTest(arg1, arg2, arg3, testHandle) });
-    })
+after(async () => {
+    await driver.quit();
 })
 
-async function assemble(name, generator) {
-    data[name] = [];
+setFormText = async (values) => {
 
-    for await (const response of generator) { data[name].push(response) }
-}
-
-function validateF10x10(a, b, c, d, e, f, g, h, i, j, testHandle) {
-    let result = [];
-
-    if (a === 'a0') {
-        result.push("Incorrect A");
-    }
-
-    if (b === 'b1') {
-        result.push("Incorrect B");
-    }
-
-    if (h === 'h6') {
-        result.push("Incorrect H");
-    }
-
-    if (result.length > 0) {
-        testHandle.addFeedback(false, { duration: getDuration(), comment: result.join(", "), custom: { Severity: `${ result.length }` } });
-        assert.fail();
-    } else {
-        testHandle.addFeedback(true, { duration: getDuration() });
+    for (let i = 0 ; i < pageFormInput[0].length ; i++) {
+        const element = await driver.findElement(By.id(pageFormInput[0][i]));
+        await element.clear();
+        await element.sendKeys(values[i]);
     }
 }
 
-function validateF100x2(a, b, testHandle) {
-    let result = [];
+setFormSelect = async (values) =>  {
+    let missingFields = [];
 
-    if (a === 'a00') {
-        result.push("Incorrect A");
+    field:
+    for (let i = 0 ; i < pageFormInput[1].length ; i++) {
+        const element = await driver.findElement(By.id(pageFormInput[1][i]));
+        const options = await element.findElements(By.tagName('option'));
+
+        for (let j = 0 ; j < options.length ; j++) {
+            if (await options[j].getText() === values[i]) {
+                await options[j].click();
+                continue field;
+            }
+        }
+
+        missingFields.push(`- Missing field: ${values[i]}.`);
     }
 
-    if (b === 'b00') {
-        result.push("Incorrect B");
-    }
+    return missingFields;
+}
 
-    if (result.length > 0) {
-        testHandle.addFeedback(false, { duration: getDuration(), comment: result.join(", "), custom: { Severity: `${ result.length }` } });
-        assert.fail();
-    } else {
-        testHandle.addFeedback(true, { duration: getDuration() });
+execute = async () => {
+
+    for (let i = 0 ; i < pageFormControl.length ; i++) {
+        const element = await driver.findElement(By.id(pageFormControl[i]));
+        await element.click();
     }
 }
 
-function validateFTest(arg1, arg2, arg3, testHandle) {
-    let result = [];
+getResponse = async () => {
+    let results = [];
 
-    if (arg1 >= 2) {
-        result.push("Incorrect 'arg1'");
+    const status = await driver.findElement(By.id(pageFormOutput[0]));
+    const statusValue = await status.getAttribute("value")
+
+    if (statusValue.includes('rejected')) {
+        const response = await driver.findElement(By.id(pageFormOutput[1]));
+        const responseValue = await response.getAttribute("value")
+
+        results.push(responseValue); 
     }
 
-    if (result.length > 0) {
-        testHandle.addFeedback(false, { duration: getDuration(), comment: result.join(", "), custom: { Severity: `${ result.length }` } });
-        assert.fail();
-    } else {
-        testHandle.addFeedback(true, { duration: getDuration() });
-    }
+    return results;
 }
 
-function getDuration() {
+it('API tests', () => {
+    describe('API', async () => {
+        forEach(data).it('API', async (country, name, address, product, color, size, quantity, payment, delivery, phone, email, testHandle) => {
 
-    return 100 + Math.floor(Math.random() * 400);
-}
+            await setFormText([name, address, quantity, phone, email]);
+            let inputError = await setFormSelect([country, product, color, size, payment, delivery]);
+            await execute();
+            let outputError = await getResponse();
+
+            let index = 1;
+            let custom = {};
+
+            if (inputError.length > 0) {
+                inputError.forEach(e => { custom[`${index++}`] = e.substring(2); })
+                testHandle.addFeedback(false, { custom, comment: "Input" });
+                assert.fail();
+            }
+
+            if (outputError.length > 0) {
+                outputError.forEach(e => { custom[`${index++}`] = e.substring(2); })
+                testHandle.addFeedback(false, { custom, comment: "Output" });
+                assert.fail();
+            }
+
+            testHandle.addFeedback(true);
+        });
+    });
+})
 
