084cd06a9d636f2f1839b283017d13509961ea50
krzysztof skorupski <k.skorupski@testify.no>
2021-07-09 15:18:46 +0200

    python api

--- workshop/PyTest/workshop_default_api.py
+++ workshop/PyTest/workshop_default_api.py
@@ -1,8 +1,8 @@
-import ecfeed
-from ecfeed import TestProvider, DataSource, TemplateType
+import sys
+sys.path.append('/home/krzysztof/Desktop/git/ecfeed.python')
+from ecfeedX import TestProvider
 import pytest
 import time
-import json
 import requests
 
 # ---------------------------------------------------------
@@ -36,34 +36,47 @@ def send_request(Country, Name, Address, Product, Color, Size, Quantity, Payment
 
     return requests.post(configuration["endpoint"], params=parameters)
 
+def parse_to_dictionary(array):
+    index = 1
+    results = {}
+    
+    for x in array:
+        results[str(index)] = x
+        index += 1
+
+    return results
+
 def process_request(Country, Name, Address, Product, Color, Size, Quantity, Payment, Delivery, Phone, Email):
     
     timeStart = time.time()
     response = send_request(Country, Name, Address, Product, Color, Size, Quantity, Payment, Delivery, Phone, Email)
     timeEnd = time.time()
-
+    
     return {
         "response": response,
         "time": int(1000 * (timeEnd - timeStart)),
         "error": {
-            "input": response.json()['errorInput'],
-            "output": response.json()['errorOutput']
+            "input": parse_to_dictionary(response.json()['errorInput']),
+            "output": parse_to_dictionary(response.json()['errorOutput'])
         }
     }
 
-def process_response(response, test_id):
+def process_response(response, test_handle):
+
+    assert len(response["error"]["input"]) == 0, test_handle.add_feedback(
+        False, duration=response["time"], comment="Input", custom=response["error"]["input"])
     
-    assert len(response["error"]["input"]) == 0, ecfeed.feedback(test_id, False, duration=response["time"], comment=''.join('- ' + e + ' ' for e in response["error"]["input"]), custom={'Error type': 'Input'})
-    assert len(response["error"]["output"]) == 0, ecfeed.feedback(test_id, False, duration=response["time"], comment=''.join('- ' + e + ' ' for e in response["error"]["output"]), custom={'Error type': 'Output'})
+    assert len(response["error"]["output"]) == 0, test_handle.add_feedback(
+        False, duration=response["time"], comment="Output", custom=response["error"]["output"])
     
-    ecfeed.feedback(test_id, True, duration=response["time"])
+    test_handle.add_feedback(True, duration=response["time"])
 
 # ---------------------------------------------------------
 
-@pytest.mark.parametrize(ecfeed.test_header(method, feedback=True), ecfeed.nwise(method=method, feedback=True))
-def test_method_nwise(Country, Name, Address, Product, Color, Size, Quantity, Payment, Delivery, Phone, Email, test_id):
+@pytest.mark.parametrize(ecfeed.test_header(method, feedback=True), ecfeed.nwise(method=method, feedback=True, label="API", constraints="NONE"))
+def test_method_nwise(Country, Name, Address, Product, Color, Size, Quantity, Payment, Delivery, Phone, Email, test_handle):
 
     response = process_request(Country, Name, Address, Product, Color, Size, Quantity, Payment, Delivery, Phone, Email)
-    process_response(response, test_id)
+    process_response(response, test_handle)
 
     
