c9842fb05e02e6bd62ad2489d99a91cb39842840
Krzysztof Skorupski <k.skorupski@testify.no>
2023-07-26 14:56:35 +0200

    cypress update

--- workshop/cypress/cypress.config.js
+++ workshop/cypress/cypress.config.js
@@ -3,6 +3,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "wuzirn",
+    baseUrl: 'http://www.workshop-2021-december.ecfeed.com?mode=error',
+    viewportWidth: 1024,
+    viewportHeight: 910,
     supportFile: false,
     setupNodeEvents(on, config) {
       const webpackDefaults = webpackPreprocessor.defaultOptions;
--- workshop/cypress/cypress/e2e/csv.cy.js
+++ workshop/cypress/cypress/e2e/csv.cy.js
@@ -1,31 +1,30 @@
-const source = require("../source/source.csv");
+/// <reference types="Cypress" />
 
-describe('ecFeed demo', () => {
+const source = require("../fixtures/source.csv");
+
+describe('EcFeed', () => {
 
   source.forEach((element, index) => {
 
-    context(`Test case: ${index}.`, () => {
+    it(`should process an order ${index}`,  () => {
+      cy.visit('/');
 
-      it('tutorial',  () => {
-        cy.visit('http://www.workshop-2021-december.ecfeed.com?mode=error');
+      cy.get('#quantity').type(element.Quantity);
+      cy.get('#name').type(element.Name);
+      cy.get('#address').type(element.Address);
+      cy.get('#phone').type(element.Phone);
+      cy.get('#email').type(element.Email);
 
-        cy.get('#quantity').type(element.Quantity);
-        cy.get('#name').type(element.Name);
-        cy.get('#address').type(element.Address);
-        cy.get('#phone').type(element.Phone);
-        cy.get('#email').type(element.Email);
+      cy.get('#product').select(element.Product);
+      cy.get('#color').select(element.Color);
+      cy.get('#size').select(element.Size);
+      cy.get('#country').select(element.Country);
+      cy.get('#payment').select(element.Payment);
+      cy.get('#delivery').select(element.Delivery);
 
-        cy.get('#product').select(element.Product);
-        cy.get('#color').select(element.Color);
-        cy.get('#size').select(element.Size);
-        cy.get('#country').select(element.Country);
-        cy.get('#payment').select(element.Payment);
-        cy.get('#delivery').select(element.Delivery);
+      cy.get('#submit').click();
 
-        cy.get('#submit').click();
-
-        cy.get('#response').should('have.value', 'Order processed without errors! Please wait for the delivery...')
-      })
-    })
-  })
-})
+      cy.get('#response').contains('Order processed without errors!')
+    });
+  });
+});
